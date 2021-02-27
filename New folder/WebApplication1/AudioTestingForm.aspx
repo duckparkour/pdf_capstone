bba5e93ee3be645b8a328d6b44d82561434ff34a
jjmasters <47305282+jjmasters@users.noreply.github.com>
2021-02-27 11:38:57 -0500

    Work on Audio/Video recording feature
    Created AudioTestingForm webform as a test environment for this portion of the project.

---
+++ New folder/WebApplication1/AudioTestingForm.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AudioTestingForm.aspx.cs" Inherits="WebApplication1.AudioTestingForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:Button ID="1" runat="server" Text="Record" OnClick="RecordButton" />
+
+             <asp:Button ID="2" runat="server" Text="Stop" OnClick="StopButton" />
+
+             <asp:Button ID="3" runat="server" Text="Stop" OnClick="PlayButton" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ New folder/WebApplication1/AudioTestingForm.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.Collections;
+using System.Drawing;
+using Microsoft.VisualBasic;
+using System.Data;
+using System.Runtime.InteropServices;
+
+namespace WebApplication1
+{
+    public partial class AudioTestingForm : System.Web.UI.Page
+    {
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int record(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void RecordButton(System.Object sender, System.EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+            record("open new Type waveaudio Alias recsound", "", 0, 0);
+            record("record recsound", "", 0, 0);
+        }
+
+        public void StopButton(System.Object sender, System.EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Enabled = false;
+            record("save recsound d:\\mic.wav", "", 0, 0);
+            record("close recsound", "", 0, 0);
+        }
+
+        public void PlayButton(System.Object sender, System.EventArgs e)
+        {
+            ms = 0;
+            h = 0;
+            s = 0;
+            m = 0;
+            timer1.Enabled = false;
+            lblhur.Text = "00";
+            lblmin.Text = "00";
+            lblsecond.Text = "00";
+            (new Microsoft.VisualBasic.Devices.Audio()).Play("d:\\mic.wav");
+        }
+    }
+}
