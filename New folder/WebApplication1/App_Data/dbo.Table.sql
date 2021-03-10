7570d1a8b98529a1b25abc73fced2a8561f77b7e
Chris Stubbs <chris.stubbs@neenah.com>
2021-03-09 21:48:17 -0500

    razor page with entity framework database

--- New folder/WebApplication1/App_Data/dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[FileID] INT NOT NULL PRIMARY KEY,
-	[FileExtension] VARCHAR(10) NOT NULL ,
-	[FileName] VARCHAR(200) NOT NUll,
-	[FileSize] INT NOT NULL,
-	[ContentType] VARCHAR(200) NOT NULL,
-	[FileContent] VARBINARY(MAX) NOT NULL,
-)
