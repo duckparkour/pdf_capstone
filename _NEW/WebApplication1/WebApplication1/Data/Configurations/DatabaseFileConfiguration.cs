fa6b8f04e7129a9e41bb26a69cacc03904ddcf54
jjmasters <47305282+jjmasters@users.noreply.github.com>
2021-03-17 21:55:17 -0400

    Finished Creating new Test Database
    Created test database to load and store files. Added appropriate models as well so everything should update and work smoothly.

---
+++ _NEW/WebApplication1/WebApplication1/Data/Configurations/DatabaseFileConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Data.Configurations
+{
+    public class DatabaseFileConfiguration : IEntityTypeConfiguration<DatabaseFile>
+    {
+        public void Configure(EntityTypeBuilder<DatabaseFile> builder)
+        {
+            builder.Property(p => p.FileID).HasColumnName("FileIDNumber");
+            builder.Property(p => p.ContentType).HasColumnName("FileContentType");
+            builder.Property(p => p.FileContent).HasColumnName("FileContent");
+            builder.Property(p => p.FileExtension).HasColumnName("FileExtensionType");
+            builder.Property(p => p.FileName).HasColumnName("Name");
+            builder.Property(p => p.FileSize).HasColumnName("SizeOfFile");
+        }
+    }
+}
