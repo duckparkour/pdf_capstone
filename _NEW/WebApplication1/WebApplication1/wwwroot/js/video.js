434b7fcd8366d62609d49b62810c46349f669cd2
David Hall <david@davidhalldevelops.com>
2021-03-21 19:56:55 -0400

    fix

--- _NEW/WebApplication1/WebApplication1/wwwroot/js/video.js
+++ _NEW/WebApplication1/WebApplication1/wwwroot/js/video.js
@@ -1,25 +1,6 @@
 $(document).ready(function(){
 
     var trailer = "?enablejsapi=1";
-    // This code loads the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    //This function creates an <iframe> (and YouTube player)after the API code downloads.
-    var player;
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player('#youtube-video', {
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
-        });
-    }
-    //Launch video as it's ready
-    function onPlayerReady(event) {
-        event.target.playVideo();
-    }
     $(".main").on('click', '#youtube-search-button', function (e) {
         e.preventDefault();
         let source = $('#youtube-search-bar').val();
