0a002527adc6912f85525fd12aa7324e9120aeb4
jjmasters <47305282+jjmasters@users.noreply.github.com>
2021-03-29 00:19:13 -0400

    Added Upload Functionality. Broke Project
    Please help.

--- _NEW/WebApplication1/WebApplication1/Pages/Index.cshtml.cs
+++ _NEW/WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using WebApplication1.Data;
 using WebApplication1.Models;
@@ -13,20 +16,49 @@ namespace WebApplication1.Pages
         /*        private readonly ILogger<IndexModel> _logger;
         */
         //private readonly AudioContext db;
-       // public IndexModel(AudioContext db) => this.db = db;
-       // public List<AudioModel> Audios { get; set; } = new List<AudioModel>();
+        // public IndexModel(AudioContext db) => this.db = db;
+        // public List<AudioModel> Audios { get; set; } = new List<AudioModel>();
 
         private readonly DatabaseFileContext db1;
 
         public IndexModel(DatabaseFileContext db1) => this.db1 = db1;
 
-       public List<DatabaseFile> FileDatabase { get; set; } = new List<DatabaseFile>();
+        public IHostingEnvironment hostingEnvironment;
+        public List<DatabaseFile> FileDatabase { get; set; } = new List<DatabaseFile>();
 
+
+        /*
+        public IndexModel(IHostingEnvironment environment)
+        {
+            this.hostingEnvironment = environment;
+        }
+        */
+        
         public async Task OnGetAsync()
         {
             //Audios = await db.Audios.ToListAsync();
             FileDatabase = await db1.Files.ToListAsync();
+            //FileDatabase = await db1.Files.AddAsync();
+            //await db1.SaveChangesAsync();
         }
+
+        public void OnPost(IFormFile formFile)
+        {
+            
+            BinaryReader br = new BinaryReader(formFile.OpenReadStream());
+            byte[] buffer = br.ReadBytes((int)formFile.Length);
+            DatabaseFile uploadedFile = new DatabaseFile();
+            uploadedFile.FileName = formFile.FileName;
+            uploadedFile.ContentType = formFile.ContentType;
+            uploadedFile.FileID = 2;
+            uploadedFile.FileSize = (int)formFile.Length;
+            uploadedFile.FileExtension = Path.GetExtension(formFile.FileName);
+            uploadedFile.FileContent = buffer;
+            db1.Add(uploadedFile);
+            db1.SaveChanges();
+            
+        }
+
 
         public void FileDownload()
         { 
@@ -53,4 +85,5 @@ namespace WebApplication1.Pages
     */
 
     }
+
 }
